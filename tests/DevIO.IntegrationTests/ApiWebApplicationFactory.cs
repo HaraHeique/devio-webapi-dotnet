4377ba1a6d794485691f57b415227122684d0d30
HaraHeique <heikacademicos@hotmail.com>
2021-12-11 19:51:03 -0300

    test: adição da configuration para permitir obter a connection string do app.testing.settings.json

--- tests/DevIO.IntegrationTests/ApiWebApplicationFactory.cs
+++ tests/DevIO.IntegrationTests/ApiWebApplicationFactory.cs
@@ -1,13 +1,20 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
 
 namespace DevIO.IntegrationTests
 {
     public class ApiWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
     {
+        public IConfiguration Configuration { get; private set; }
+
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
             builder.UseEnvironment("Testing");
+
+            Configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.Testing.json")
+                .Build();
         }
     }
 }
